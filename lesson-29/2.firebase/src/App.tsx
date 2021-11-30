d6903c754d4637dfb2ad008716fea8994e43c1d9
AndreySFNMaxima <AndreySFNMaxima@yandex.ru>
2021-11-30 17:15:02 +0300

    Занятие 29. Часть 2. Firebase

---
+++ lesson-29/2.firebase/src/App.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import i18next from 'i18next';
+import React, { useEffect, useState } from 'react';
+import './App.less';
+import { Trans, useTranslation } from 'react-i18next';
+import './locale/i18next';
+
+const App = () => {
+  const [userName, setUserName] = useState('');
+
+  const { t } = useTranslation();
+
+  const handleChangeLanguage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    i18next.changeLanguage(e.currentTarget.value);
+  };
+
+  // eslint-disable-next-line no-restricted-globals,no-alert
+  // @ts-ignore
+  // eslint-disable-next-line no-alert
+  useEffect(() => setUserName(
+    // prompt(t('nameQuestion'))
+    'Andreas',
+  ),
+  []);
+
+  return (
+    <div id="app">
+      <h1>{t('helloMessage', { name: userName })}</h1>
+      <Trans
+        i18nKey="descriptionMessage"
+        values={{ name: userName }}
+        components={{ h2: <h2 /> }}
+      >Значение по умолчанию
+      </Trans>
+      <div className="date-now">
+        {t('date_format', { date: new Date() })}
+      </div>
+      <div>
+        {t('plurals.apple', { count: 0 })}
+      </div>
+      <div>
+        {t('plurals.apple', { count: 1 })}
+      </div>
+      <div>
+        {t('plurals.apple', { count: 2 })}
+      </div>
+      <div className="language-buttons">
+        <button value="en" type="button" onClick={handleChangeLanguage}>EN</button>
+        <button value="ru" type="button" onClick={handleChangeLanguage}>RU</button>
+        <button value="no" type="button" onClick={handleChangeLanguage}>NO</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
