3094d7c16c7edf0558c3ff54fa6f3b736f278ed1
AndreySFNMaxima <AndreySFNMaxima@yandex.ru>
2021-11-30 17:15:02 +0300

    Занятие 29. Часть 2. Firebase

---
+++ lesson-29/2.firebase/src/firebase.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDjEBZpgTjqs2qQZsi_EjGZAHHyvjL-CmM',
+  authDomain: 'andreysfn-my-first-app.firebaseapp.com',
+  projectId: 'andreysfn-my-first-app',
+  storageBucket: 'andreysfn-my-first-app.appspot.com',
+  messagingSenderId: '95441542355',
+  appId: '1:95441542355:web:e0ab778e7ded197204c826',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
